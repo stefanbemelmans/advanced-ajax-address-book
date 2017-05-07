630465c3b5b116f0cdad45e95234b9884855458f
Jon Woo <jw56578@gmail.com>
2017-05-07 13:41:20 -0500

    finish

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import UserDetailContainer from "./containers/UserDetailContainer";
 import ListOfUsersContainer from "./containers/ListOfUsersContainer";
 import SearchBox from "./components/SearchBox";
 
-function App(props) {
-  return (
-    <div>
-      <SearchBox />
-      <ListOfUsersContainer />
-      <UserDetailContainer />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+  }
+  componentDidMount() {
+
+  }
+  render() {
+    return (
+      <div>
+        <SearchBox />
+        <ListOfUsersContainer />
+        <UserDetailContainer />
+      </div>
+    );
+  }
 }
 
 export default App;
