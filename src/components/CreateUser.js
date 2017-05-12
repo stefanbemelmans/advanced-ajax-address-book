d22aa8193abd33102ad344adc38a5d0b3a680477
Jon Woo <jw56578@gmail.com>
2017-05-12 17:41:07 -0500

    add create user

---
+++ src/components/CreateUser.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class CreateUser extends Component {
+  constructor(){
+    super();
+    this.state =  {visible:true};
+  }
+  render() {
+    return (
+      <div>
+        
+        
+      </div>);
+  }
+}
+
+export default (CreateUser);
